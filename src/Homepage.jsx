609e9ee30c8ca78d870f034557487644df3d916b
renatosanz <renato.sanchez3103@gmail.com>
2024-07-25 10:23:02 -0600

    dark/light mode detection fixed

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,49 +1,57 @@
-import React, {useState, useRef } from 'react'
+import React, { useState, useRef,useEffect } from "react";
 
-import "./page/themeDark.css"
-import "./page/themeLight.css"
+import "./page/themeDark.css";
+import "./page/themeLight.css";
 
-import Navbar from './page/Navbar';
-import Description from './Homepage/Description';
-import WhatIDo from './Homepage/WhatIDo';
-import MyWork from './Homepage/MyWork';
-import AboutMe from './Homepage/AboutMe';
-import Footer from './page/Footer';
-import BlogList from './components_Blog/BlogList';
-import Playground from './playground/Playground';
+import Navbar from "./page/Navbar";
+import Description from "./Homepage/Description";
+import WhatIDo from "./Homepage/WhatIDo";
+import MyWork from "./Homepage/MyWork";
+import AboutMe from "./Homepage/AboutMe";
+import Footer from "./page/Footer";
+import BlogList from "./components_Blog/BlogList";
+import Playground from "./playground/Playground";
 
 export default function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
   const what_i_do = useRef(null);
   const my_work = useRef(null);
   const about_me = useRef(null);
-  
+
   const home = useRef(null);
   const blog = useRef(null);
   const playground = useRef(null);
 
-
-
-  const changeTheme=()=>{
-    if(theme==="dark"){
-      setTheme("light")
-    }else{
-      setTheme("dark")
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
     }
-  }
+  }, [])
+  
+
+  const changeTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
 
   return (
-    <div id='background' className={"mode-"+theme}>
-      <Navbar func={changeTheme} icon={theme} refs={[home,blog,playground]}/>
-      <div id='cont-page'>
-        <Description selfRef={home} refs={{what_i_do,my_work,about_me}}/>
-        <WhatIDo selfRef={what_i_do}/>
-        <MyWork selfRef={my_work}/>
-        <AboutMe selfRef={about_me}/>
-        <BlogList selfRef={blog}/>
-        <Playground selfRef={playground}/>
+    <div id="background" className={"mode-" + theme}>
+      <Navbar func={changeTheme} icon={theme} refs={[home, blog, playground]} />
+      <div id="cont-page">
+        <Description selfRef={home} refs={{ what_i_do, my_work, about_me }} />
+        <WhatIDo selfRef={what_i_do} />
+        <MyWork selfRef={my_work} />
+        <AboutMe selfRef={about_me} />
+        <BlogList selfRef={blog} />
+        <Playground selfRef={playground} />
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,40 +1,123 @@
-
 export const mySkills = [
-  {name:"Python",src_img:"https://img.icons8.com/?size=100&id=13441&format=png&color=000000"},
-  {name:"Lua",src_img:"https://img.icons8.com/?size=100&id=utx96pPa33XK&format=png&color=000000"},
-  {name:"HTML5",src_img:"https://img.icons8.com/?size=100&id=20909&format=png&color=000000"},
-  {name:"CSS",src_img:"https://img.icons8.com/?size=100&id=21278&format=png&color=000000"},
-  {name:"JS",src_img:"https://img.icons8.com/?size=100&id=108784&format=png&color=000000"},
-  {name:"Node-JS",src_img:"https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000"},
-  {name:"Git",src_img:"https://img.icons8.com/?size=100&id=20906&format=png&color=000000"},
-  {name:"React-JS",src_img:"https://img.icons8.com/?size=100&id=123603&format=png&color=000000"},
-  {name:"AstroJS",src_img:"https://img.icons8.com/?size=100&id=kXuRhjMIeKhk&format=png&color=000000"},
-] 
+  {
+    name: "Python",
+    src_img:
+      "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+  },
+  {
+    name: "Lua",
+    src_img:
+      "https://img.icons8.com/?size=100&id=utx96pPa33XK&format=png&color=000000",
+  },
+  {
+    name: "HTML5",
+    src_img:
+      "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
+  },
+  {
+    name: "CSS",
+    src_img:
+      "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+  },
+  {
+    name: "JS",
+    src_img:
+      "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+  },
+  {
+    name: "Node-JS",
+    src_img:
+      "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000",
+  },
+  {
+    name: "Git",
+    src_img:
+      "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+  },
+  {
+    name: "React-JS",
+    src_img:
+      "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
+  },
+  {
+    name: "AstroJS",
+    src_img:
+      "https://img.icons8.com/?size=100&id=kXuRhjMIeKhk&format=png&color=000000",
+  },
+];
 
- const calc = {
-  url:"https://calc-renato.netlify.app",
-  bgColor:"#d68100",
-  fontColor:"#1E1E1E",
-  title:"Calc",
-  icon:"https://raw.githubusercontent.com/renatosanz/Calc/df5b4bd8c66ec0b7632f78480cc1fc4ca4239548/src/images/icon.svg",
-  repository:"https://github.com/renatosanz/Calc"
-}
- const newsApp = {
-  url:"https://news-app-renato.netlify.app",
-  bgColor:"#f3f3f3",
-  fontColor:"#1e1e1e",
-  title:"News App",
-  icon:"https://raw.githubusercontent.com/renatosanz/news-APP/8c99ca1a5aba767843133e89acc15fcbabc1711e/src/homepage/imgs/logo.svg",
-  repository:"https://github.com/renatosanz/news-APP"
-}
+const calc = {
+  url: "https://calc-renato.netlify.app",
+  bgColor: "#d68100",
+  fontColor: "#1E1E1E",
+  title: "Calc",
+  icon: "https://raw.githubusercontent.com/renatosanz/Calc/df5b4bd8c66ec0b7632f78480cc1fc4ca4239548/src/images/icon.svg",
+  repository: "https://github.com/renatosanz/Calc",
+};
+const newsApp = {
+  url: "https://news-app-renato.netlify.app",
+  bgColor: "#f3f3f3",
+  fontColor: "#1e1e1e",
+  title: "News App",
+  icon: "https://raw.githubusercontent.com/renatosanz/news-APP/8c99ca1a5aba767843133e89acc15fcbabc1711e/src/homepage/imgs/logo.svg",
+  repository: "https://github.com/renatosanz/news-APP",
+};
 
-export const myProjects = [calc,newsApp]
+const riprtx = {
+  url: "https://riprtx.netlify.app/",
+  bgColor: "#281a15",
+  fontColor: "#fefefe",
+  title: "riprtx",
+  icon: "https://img.icons8.com/?size=100&id=18911&format=png&color=000000",
+  repository: "https://github.com/renatosanz/riprtx",
+};
+const InterVisor = {
+  url: "https://github.com/renatosanz/delfin",
+  bgColor: "#427b58",
+  fontColor: "#fefefe",
+  title: "InterVisor",
+  icon: "https://img.icons8.com/?size=100&id=64353&format=png&color=000000",
+  repository: "https://github.com/renatosanz/delfin",
+};
 
+const textAnalyzer = {
+  url: "https://github.com/renatosanz/analizadorTextos",
+  bgColor: "#8f3f71",
+  fontColor: "#fefefe",
+  title: "textAnalyzer",
+  icon: "https://img.icons8.com/?size=100&id=hiVUDmBVLfG9&format=png&color=000000",
+  repository: "https://github.com/renatosanz/analizadorTextos",
+};
 
+const typo = {
+  url: "https://github.com/renatosanz/typo",
+  bgColor: "#b57614",
+  fontColor: "#fefefe",
+  title: "typo",
+  icon: "https://img.icons8.com/?size=100&id=8WgpFCLqcMzv&format=png&color=000000",
+  repository: "https://github.com/renatosanz/typo",
+};
+
+export const myProjects = [
+  calc,
+  newsApp,
+  riprtx,
+  InterVisor,
+  textAnalyzer,
+  typo,
+];
 
 export const aboutMedata = [
-  {data:"Hello! I'm Renato Sanchez, and I wear many hats - I'm a designer, a web developer, a passionate advocate for open-source projects, a devoted fan of pixel art, a computer science student, and someone who's always eager to learn."},
-  {data:"My fascination with the world of technology ignited when I was a mere 10-year-old. I still remember the excitement of laying my hands on my very first laptop, which marked the beginning of my incredible journey into the realm of computing and digital creativity."},
-  {data:"Since then, I've been on a continuous quest to expand my knowledge and skills, diving deeper into the ever-evolving world of web development and design. This journey has been marked by a profound appreciation for open-source collaboration, a creative flair for pixel art, and an unwavering commitment to honing my craft."},
-  {data:"Thank you for joining me on this exciting adventure through the digital landscape!"},
-]
+  {
+    data: "Hello! I'm Renato Sanchez, and I wear many hats - I'm a designer, a web developer, a passionate advocate for open-source projects, a devoted fan of pixel art, a computer science student, and someone who's always eager to learn.",
+  },
+  {
+    data: "My fascination with the world of technology ignited when I was a mere 10-year-old. I still remember the excitement of laying my hands on my very first laptop, which marked the beginning of my incredible journey into the realm of computing and digital creativity.",
+  },
+  {
+    data: "Since then, I've been on a continuous quest to expand my knowledge and skills, diving deeper into the ever-evolving world of web development and design. This journey has been marked by a profound appreciation for open-source collaboration, a creative flair for pixel art, and an unwavering commitment to honing my craft.",
+  },
+  {
+    data: "Thank you for joining me on this exciting adventure through the digital landscape!",
+  },
+];
